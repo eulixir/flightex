e6e8a755544cd8d5437cd1d9676ddae6174cdfec
João Pedro Alves <jotalmeida007@hotmail.com>
2021-04-04 20:58:41 -0300

    Create user function

---
+++ lib/users/user.ex
@@ -0,0 +1,22 @@
+defmodule Flightex.Users.User do
+  @keys [:name, :email, :cpf]
+  @enforce_keys @keys
+  defstruct @keys
+
+  def build(name, email, cpf) when is_bitstring(cpf) do
+    {:ok,
+    %__MODULE__{
+      name: name,
+      email: email,
+      cpf: cpf
+    }}
+  end
+
+  def build(_name, _email, cpf) when is_integer(cpf),
+    do: {:error, "Cpf must be a String"}
+
+  def build(_name, _email, _cpf), do: {:error, "Invalid Parameters"}
+  def build(_name, _email), do: {:error, "Need more Parameters"}
+  def build(_name), do: {:error, "Need more Parameters"}
+  def build(), do: {:error, "This function need parameters"}
+end
