8bce9b862da4e84136b0aa2a69355cd22baa61a2
João Pedro Alves <jotalmeida007@hotmail.com>
2021-04-03 23:31:40 -0300

    Start project

---
+++ lib/flightex.ex
@@ -0,0 +1,18 @@
+defmodule Flightex do
+  @moduledoc """
+  Documentation for `Flightex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Flightex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/flightex_test.exs
@@ -0,0 +1,8 @@
+defmodule FlightexTest do
+  use ExUnit.Case
+  doctest Flightex
+
+  test "greets the world" do
+    assert Flightex.hello() == :world
+  end
+end
