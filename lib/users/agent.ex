1d712b55b5abdaa8f0ad225b5f33e1c556d610ba
João Pedro Alves <jotalmeida007@hotmail.com>
2021-04-04 21:16:34 -0300

    Add user agent

---
+++ lib/users/agent.ex
@@ -0,0 +1,22 @@
+defmodule Flightex.Users.Agent do
+  alias Flightex.Users.User
+
+  use Agent
+
+  def start_link(%{}) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def save(%User{} = user), do: Agent.update(__MODULE__, &update_state(&1, user))
+
+  def get(cpf), do: Agent.get(__MODULE__, &get_user(&1, cpf))
+
+  defp get_user(state, cpf) do
+    case Map.get(state, cpf) do
+      nil -> {:error, "User not found"}
+      user -> {:ok, user}
+    end
+  end
+
+  defp update_state(state, %User{cpf: cpf} = user), do: Map.put(state, cpf, user)
+end
