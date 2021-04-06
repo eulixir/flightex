c3ea0038947f838f3c2bd6acb72c173bf8ab9342
João Pedro Alves <jotalmeida007@hotmail.com>
2021-04-06 14:30:36 -0300

    Create booking agent test

--- test/bookings/booking_agent_test.exs
+++ test/bookings/booking_agent_test.exs
@@ -1,1 +1,59 @@
+defmodule Flightex.Bookings.AgentTest do
+  use ExUnit.Case
 
+  import Flightex.Factory
+
+  alias Flightex.Bookings.Agent, as: BookingsAgent
+
+  describe "save/1" do
+    test "when the param are valid, return a booking uuid" do
+      BookingsAgent.start_link(%{})
+      response = BookingsAgent.save(build(:booking))
+      {_ok, uuid} = response
+
+      assert response == {:ok, uuid}
+    end
+  end
+
+  describe "get/1" do
+    setup do
+      BookingsAgent.start_link(%{})
+
+      :ok
+
+      id = UUID.uuid4()
+      {:ok, id: id}
+    end
+
+    test "when the user is found, return a booking", %{id: id} do
+      booking = build(:booking, id: id)
+      {:ok, uuid} = BookingsAgent.save(booking)
+
+      response = BookingsAgent.get(uuid)
+
+      expected_response =
+        {:ok,
+         %Flightex.Bookings.Booking{
+           complete_date: [2001, 5, 7, 3, 5, 0],
+           id: id,
+           local_destination: "Bananeiras",
+           local_origin: "Brasilia",
+           user_id: "12345678900"
+         }}
+
+      assert response == expected_response
+    end
+
+    test "when the user is't founded, return an error", %{id: id} do
+      booking = build(:booking, id: id)
+      {:ok, uuid} = BookingsAgent.save(booking)
+
+      response = BookingsAgent.get("banana")
+
+      expected_response =
+        {:error, "Booking not found"}
+
+      assert response == expected_response
+    end
+  end
+end
